0dae8979fd5a372391169bf7a7e356eb2ae19ba5
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-01-31 15:25:14 +0600

    Add solution 1.4.2

--- 1/app.js
+++ 1/app.js
@@ -5,7 +5,7 @@ button.addEventListener("click", function () {
   const input = Number(document.getElementById("number1").value);
   result.innerText = "Result:";
 
-  for (let i = 1; i <= input; i++) {
+  for (let i = input; i <= 20; i++) {
     result.innerText += ` ${i}`;
   }
 });
