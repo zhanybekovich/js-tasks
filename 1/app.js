c7dd0760e1ec14b0935765b3f99ca0f8de88f56a
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-01-31 15:17:52 +0600

    Add solution 1.4.1

--- 1/app.js
+++ 1/app.js
@@ -1,20 +1,11 @@
-const taskText =
-  "Даны два целых числа. Проверьте, что первое число без остатка делится на второе.";
-
-const h1 = document.getElementById("task");
-
-h1.innerText = taskText;
-
 const button = document.getElementById("submitBtn");
 const result = document.getElementById("result");
 
 button.addEventListener("click", function () {
-  const num1 = Number(document.getElementById("number1").value);
-  const num2 = Number(document.getElementById("number2").value);
+  const input = Number(document.getElementById("number1").value);
+  result.innerText = "Result:";
 
-  if (num1 % num2 === 0) {
-    result.innerText = "Делится";
-  } else {
-    result.innerText = "Не делится";
+  for (let i = 1; i <= input; i++) {
+    result.innerText += ` ${i}`;
   }
 });
