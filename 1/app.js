48c858443175498b654f0fc9cdec946aa7a97a13
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-01-31 15:49:41 +0600

    Add solution 1.5.4

--- 1/app.js
+++ 1/app.js
@@ -2,13 +2,8 @@ const button = document.getElementById("submitBtn");
 const result = document.getElementById("result");
 
 button.addEventListener("click", function () {
-  const input = Number(document.getElementById("number1").value);
-  result.innerText = "Result:";
-  let summ = 0;
-  for (let i = 1; i <= input; i++) {
-    if (i % 2 === 0) {
-      summ += i;
-    }
-  }
-  result.innerText = summ;
+  const number1 = Number(document.getElementById("number1").value);
+
+  const number2 = Number(document.getElementById("number2").value);
+  result.innerText = `${number1 % number2}`;
 });
