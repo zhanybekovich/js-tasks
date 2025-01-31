a9a653c2371bf2c79ea8fbe2af8d505911014814
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-01-31 15:37:18 +0600

    Add solution 1.5 -1

--- 1/app.js
+++ 1/app.js
@@ -4,10 +4,9 @@ const result = document.getElementById("result");
 button.addEventListener("click", function () {
   const input = Number(document.getElementById("number1").value);
   result.innerText = "Result:";
-
+  let summ = 0;
   for (let i = 1; i <= input; i++) {
-    if (i % 3 === 0) {
-      result.innerText += ` ${i}`;
-    }
+    summ += i;
   }
+  result.innerText = summ;
 });
